4039c6cfc92497711e56ade06f271111ad07aa0f
root <root@ip-172-31-31-13.ap-south-1.compute.internal>
2026-03-04 06:49:02 +0000

    Commiting the changes to the welcome message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from QA EKS 🚀');
+  res.send('Hello team; welcome from QA EKS 🚀');
 });
 
 app.listen(4000, () => {
