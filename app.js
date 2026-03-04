18f7a5b29f262e1b8f3bc7ed6b86008ac008b131
root <root@ip-172-31-31-13.ap-south-1.compute.internal>
2026-03-04 07:36:01 +0000

    commiting changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hey Buddy !! - from QA EKS 🚀');
+  res.send('Hey Bro !! - from QA EKS 🚀');
 });
 
 app.listen(4000, () => {
