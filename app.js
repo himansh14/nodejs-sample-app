6e92929caa2d71021bccf17e45b1976332a89a0f
Himanshu S Waghale <108952827+himansh14@users.noreply.github.com>
2026-03-09 15:44:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('My Main QA Pipeline Works - from QA EKS 🚀');
+  res.send('My Main QA Pipeline Works which works correctly - from QA EKS 🚀');
 });
 
 const PORT = process.env.PORT || 4000;
