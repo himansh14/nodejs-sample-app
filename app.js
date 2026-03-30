5f018f745f22b9a5cd19c4cf0093eb99d7e9de9c
himansh14 <972214himanshu@gmail.com>
2026-03-30 09:50:11 +0000

    commit to app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from QA EKS 🚀');
+  res.send('Hello from GitHub Actions 🚀');
 });
 
 const PORT = process.env.PORT || 4000;
